effba95686aa12bea0a671a9f32574aec6efb4d5
ana <142507092+aninhax11@users.noreply.github.com>
2024-10-30 11:02:57 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -7,7 +7,32 @@
 //chamadas
 
 criaCartao(
-    'Programação'
-    'O que é javaScript?'
+    'Programação',
+    'O que é javaScript?',
     'O javaScript é uma linguagem de programação'
+    
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+   'Literatura',
+   'Qual o nome da Saga de Arlen Fardos?',
+   'O nome é Ciclo Das Trevas'
 )
